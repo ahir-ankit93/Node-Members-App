fd23e091eef7ac665721f27cbb051e31cc9182b6
ahir-ankit93 <ahir.ankit93@gmail.com>
2019-08-16 13:10:48 +0530

    Express Members App

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name:'Ankit Ahir',
+        email:'ankit.ahir@gmail.com',
+        status:'active'
+    },
+    {
+        id: 2,
+        name:'vicky modi',
+        email:'vicky.modi@gmail.com',
+        status:'inactive'
+    },
+    {
+        id: 3,
+        name:'jhon Doe',
+        email:'Doe.Doe@gmail.com',
+        status:'active'
+    }
+];
+
+module.exports = members;
